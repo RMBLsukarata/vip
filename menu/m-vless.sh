8cd77b31fc196389974aae6c18cc022404f08614
RMBLsukarata <135566962+RMBLsukarata@users.noreply.github.com>
2023-08-08 19:50:45 +0800

    Update m-vless.sh

--- menu/m-vless.sh
+++ menu/m-vless.sh
@@ -12,7 +12,7 @@ COLBG1="$(cat /etc/casper/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ /
 ipsaya=$(curl -sS ipinfo.io/ip)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/casper9/permission/main/ipmini"
+data_ip="https://raw.githubusercontent.com/RMBLsukarata/permission/main/ipmini"
 checking_sc() {
     useexp=$(curl -sS $data_ip | grep $ipsaya | awk '{print $3}')
     if [[ $date_list < $useexp ]]; then
