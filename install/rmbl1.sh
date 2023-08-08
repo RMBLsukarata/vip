650cc1b948deedf274aa432bebb35bfb347a1efa
RMBLsukarata <135566962+RMBLsukarata@users.noreply.github.com>
2023-08-09 06:03:07 +0800

    Update rmbl1.sh

--- install/rmbl1.sh
+++ install/rmbl1.sh
@@ -1,50 +1,48 @@
-#!/bin/bash 
- # // String / Request Data 
- # Getting 
- MYIP=$(wget -qO- ipinfo.io/ip); 
- clear 
- apt install jq curl -y 
- sub=$(</dev/urandom tr -dc a-z | head -c4) 
- DOMAIN=yogzvpn.cloud 
- SUB_DOMAIN=${sub}.yogzvpn.cloud 
- CF_ID=ysshvpn@gmail.com 
- CF_KEY=0d626234700bad388d6d07b49c42901445d1c 
- set -euo pipefail 
- IP=$(curl -sS ifconfig.me); 
- echo "Updating DNS for ${SUB_DOMAIN}..." 
- ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \ 
-      -H "X-Auth-Email: ${CF_ID}" \ 
-      -H "X-Auth-Key: ${CF_KEY}" \ 
-      -H "Content-Type: application/json" | jq -r .result[0].id) 
-  
- RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \ 
-      -H "X-Auth-Email: ${CF_ID}" \ 
-      -H "X-Auth-Key: ${CF_KEY}" \ 
-      -H "Content-Type: application/json" | jq -r .result[0].id) 
-  
- if [[ "${#RECORD}" -le 10 ]]; then 
-      RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \ 
-      -H "X-Auth-Email: ${CF_ID}" \ 
-      -H "X-Auth-Key: ${CF_KEY}" \ 
-      -H "Content-Type: application/json" \ 
-      --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id) 
- fi 
-  
- RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \ 
-      -H "X-Auth-Email: ${CF_ID}" \ 
-      -H "X-Auth-Key: ${CF_KEY}" \ 
-      -H "Content-Type: application/json" \ 
-      --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}') 
-       
- echo "Host : $SUB_DOMAIN" 
- echo $SUB_DOMAIN > /root/domain 
- echo "IP=$SUB_DOMAIN" > /var/lib/scrz-prem/ipvps.conf 
- sleep 1 
- yellow() { echo -e "\\033[33;1m${*}\\033[0m"; } 
- yellow "Domain added.." 
- sleep 3 
- domain=$(cat /root/domain) 
- cp -r /root/domain /etc/xray/domain 
- read -n 1 -s -r -p "Press any key to back on genssl" 
-  
- genssl 
+#!/bin/bash
+
+MYIP=$(wget -qO- icanhazip.com);
+apt install jq curl -y
+
+DOMAIN=yogzvpn.cloud
+sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
+#subsl=$(</dev/urandom tr -dc a-z0-9 | head -c5)
+dns=${sub}.yogzvpn.cloud
+#NS_dns=slowdns-${subsl}.kuhing.my.id
+CF_ID=ysshvpn@gmail.com
+CF_KEY=0d626234700bad388d6d07b49c42901445d1c
+set -euo pipefail
+IP=$(wget -qO- icanhazip.com);
+echo "Updating DNS for ${dns}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${dns}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}')
+#echo "$dns" > /root/scdomain
+echo $dns > /root/domain
+#echo "$dns" > /etc/xray/scdomain
+echo "$dns" > /etc/xray/domain
+echo "$dns" > /etc/v2ray/domain
+#echo $dns > /root/domain
+echo "IP=$dns" > /var/lib/ipvps.conf
+#echo "Host NS : $NS_DOMAIN"
+#echo $NS_dns > /root/nsdomain
+cd
