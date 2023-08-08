d677fa135d01cc5f38dfc0284e9fe001a9ee249f
RMBLsukarata <135566962+RMBLsukarata@users.noreply.github.com>
2023-08-09 07:39:45 +0800

    Update running.sh

--- menu/running.sh
+++ menu/running.sh
@@ -283,5 +283,5 @@ echo -e "❇️ Websocket None TLS      :$swsdrop"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
-
+restart
 menu
