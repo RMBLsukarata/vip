bb64c9a93a24740caa87db2649ba51c6206106ed
RMBLsukarata <135566962+RMBLsukarata@users.noreply.github.com>
2023-09-02 18:45:20 +0800

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,55 +1,47 @@
 #!/bin/bash
 clear
 red='\e[1;31m'
-green2='\e[1;32m'
+green='\e[1;32m'
 yell='\e[1;33m'
 NC='\e[0m'
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
+if [[ -e /etc/debian_version ]]; then
+	source /etc/os-release
+	OS=$ID # debian or ubuntu
+elif [[ -e /etc/centos-release ]]; then
+	source /etc/os-release
+	OS=centos
+fi
 
-echo "           ${green}Tools install...!"
-echo "                  ${green}Progress..."
-sleep 0.5
 
-sudo apt update -y
-sudo apt update -y
-sudo apt dist-upgrade -y
-apt -y install shc
-cd /root
-gem install lolcat
-sleep 1
 
-sudo apt install netfilter-persistent -y
-sudo apt-get remove --purge ufw firewalld -y 
-sudo apt-get remove --purge exim4 -y 
 
-apt-get install figlet -y
-apt-get install ruby -y
-gem install lolcat
-apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl
-apt -y install squid
-apt -y install nmap
+echo "Tools install...!"
+echo "Progress..."
+sleep 2
 
-apt autoclean -y >/dev/null 2>&1
-apt -y remove --purge unscd >/dev/null 2>&1
-apt-get -y --purge remove samba* >/dev/null 2>&1
-apt-get -y --purge remove apache2* >/dev/null 2>&1
-apt-get -y --purge remove bind9* >/dev/null 2>&1
-apt-get -y remove sendmail* >/dev/null 2>&1
-apt autoremove -y >/dev/null 2>&1
+apt update -y
+apt install python -y
+apt update -y
+apt dist-upgrade -y
+apt install sudo -y
+apt-get remove --purge ufw firewalld -y 
+apt-get remove --purge exim4 -y 
 
-sudo apt install -y screen curl jq bzip2 gzip coreutils rsyslog iftop \
- htop zip unzip net-tools sed gnupg gnupg1 \
- bc sudo apt-transport-https build-essential dirmngr libxml-parser-perl neofetch screenfetch git lsof \
- openssl openvpn easy-rsa fail2ban tmux \
- stunnel4 vnstat squid3 \
- dropbear  libsqlite3-dev \
- socat cron bash-completion ntpdate xz-utils sudo apt-transport-https \
- gnupg2 dnsutils lsb-release chrony
 
-curl -sSL https://deb.nodesource.com/setup_16.x | bash - 
-sudo apt-get install nodejs -y
+apt install -y screen curl jq bzip2 gzip coreutils rsyslog iftop \
+htop zip unzip net-tools sed gnupg gnupg1 \
+bc sudo apt-transport-https build-essential dirmngr libxml-parser-perl neofetch screenfetch git lsof \
+openssl openvpn easy-rsa fail2ban tmux \
+stunnel4 vnstat squid3 \
+dropbear  libsqlite3-dev \
+socat cron bash-completion ntpdate xz-utils sudo apt-transport-https \
+gnupg2 dnsutils lsb-release chrony
+
+#curl -sSL https://deb.nodesource.com/setup_16.x | bash - 
+apt-get install nodejs -y
 
 /etc/init.d/vnstat restart
 wget -q https://humdi.net/vnstat/vnstat-2.6.tar.gz
@@ -65,10 +57,12 @@ systemctl enable vnstat
 rm -f /root/vnstat-2.6.tar.gz >/dev/null 2>&1
 rm -rf /root/vnstat-2.6 >/dev/null 2>&1
 
-sudo apt install -y libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev xl2tpd pptpd
+
+fi
+
+apt install -y libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev xl2tpd pptpd
 
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 yellow "Dependencies successfully installed..."
-sleep 1
+sleep 3
 clear
-
